39657fafb224d9c8aaaa9e7c02aac58fdf3b17ac
GaroPlay <garokes0@gmail.com>
2025-11-30 22:26:13 +0300

    Init Steam Version

---
+++ Source/MultiplayerBattle/MultiplayerBattle.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MultiplayerBattle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiplayerBattle, "MultiplayerBattle" );
